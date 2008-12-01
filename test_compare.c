295e3b62a6d03e8fa8639bc6113129619ca1e660
Vitaly Lipatov <lav@etersoft.ru>
2008-12-01 14:56:28 +0300

    improve CompareString test

--- test_compare.c
+++ test_compare.c
@@ -22,60 +22,78 @@
 #include "eterforcetest.h"
 
 
-#define TEST_STRING "ABCDEFGHRTPENDOFPROZMIY"
 static WCHAR test_stringW[] = {'A','B','C','D','E','F','G','H','R','T','P','E','N','D','O','F','P','R','O','Z','M','I','Y', '\0'};
 #define TEST_STRINGW test_stringW
 
 static WCHAR test_string2W[] = {'a','b','c','d','e','f','g','h','r','t','g','o','\0'};
 #define TEST_STRING2W test_string2W
 
+static CHAR test1_string[] = "ABCDEFGHRTPENDOFPROZMIY";
+#define TEST_STRING test1_string
+
+static CHAR test1_string2[] = "abcdefghrtgo";
+#define TEST_STRING2 test1_string2
+
+
+static void test_compare_lang(int lang, const char*langname)
+{
+	static int res;
+	char name[100];
+	LCID lcid = MAKELCID(MAKELANGID(lang, SUBLANG_DEFAULT), SORT_DEFAULT);
+
+	sprintf(name, "CompareStringA/%s", langname + strlen("LANG_"));
+	MSTART(lang, name, 20) {
+		res = CompareStringA(lcid, 0, TEST_STRING, -1, TEST_STRING2, -1);
+	} MEND
+
+	sprintf(name, "CompareStringA/ICase/%s", langname + strlen("LANG_"));
+	MSTART(lang, name, 6) {
+		res = CompareStringA(lang, NORM_IGNORECASE|LOCALE_USE_CP_ACP, TEST_STRING, -1, TEST_STRING2, -1);
+	} MEND
+}
+#define TEST_COMPARE_LANG(lang) \
+	test_compare_lang(lang, ""#lang);
+
 void test_compare()
 {
 	int res;
 	#define BUFLEN 100
-	WCHAR destW[BUFLEN];
-	char dest[BUFLEN];
+	static WCHAR destW[BUFLEN];
+	static char dest[BUFLEN];
 
 	printf("\n\n* * *  Comparing operations  * * *\n");
 
 	lstrcpyA(dest, TEST_STRING);
 	MSTART(1, "lstrcmpiA", 2) {
-		res = lstrcmpiA(dest, "abcdefghrtgo");
+		res = lstrcmpiA(dest, TEST_STRING2);
 	} MEND
 
 	MSTART(1, "lstrcmpA", 2) {
-		res = lstrcmpA(dest, "abcdefghrtgo");
+		res = lstrcmpA(dest, TEST_STRING2);
 	} MEND
 
-	MSTART(1, "CompareStringA/NEUTRAL", 20) {
-		res = CompareStringA(LOCALE_NEUTRAL, 0, dest, -1, "abcdefghrtgo", -1);
+	TEST_COMPARE_LANG(LANG_BELARUSIAN);
+	TEST_COMPARE_LANG(LANG_ESTONIAN);
+	TEST_COMPARE_LANG(LANG_LATVIAN);
+	TEST_COMPARE_LANG(LANG_RUSSIAN)
+	TEST_COMPARE_LANG(LANG_UKRAINIAN);
+	TEST_COMPARE_LANG(LANG_ENGLISH);
+	TEST_COMPARE_LANG(LANG_FRENCH);
+
+	MSTART(1, "CompareStringA/CURRENT", 20) {
+		res = CompareStringA(GetThreadLocale(), 0, dest, -1, TEST_STRING2, -1);
 	} MEND
 
-	MSTART(1, "CompareStringA/cp1251", 20) {
-		res = CompareStringA(LOCALE_SYSTEM_DEFAULT, 0, dest, -1, "abcdefghrtgo", -1);
+	MSTART(1, "CompareStringA/ICase/CURRENT", 6) {
+		res = CompareStringA(GetThreadLocale(), NORM_IGNORECASE|LOCALE_USE_CP_ACP, dest, -1, TEST_STRING2, -1);
 	} MEND
 
-	MSTART(1, "CompareStringA/cp1256", 20) {
-		res = CompareStringA(LOCALE_SYSTEM_DEFAULT, 0, dest, -1, "abcdefghrtgo", -1);
-	} MEND
-
-
-	MSTART(1, "CompareStringA/NEUTRAL/IgnoreCase", 6) {
-		res = CompareStringA(LOCALE_NEUTRAL, NORM_IGNORECASE|LOCALE_USE_CP_ACP, dest, -1, "abcdefghrtgo", -1);
-	} MEND
-
-	MSTART(1, "CompareStringA/cp1251/IgnoreCase", 6) {
-		res = CompareStringA(LOCALE_SYSTEM_DEFAULT, NORM_IGNORECASE|LOCALE_USE_CP_ACP, dest, -1, "abcdefghrtgo", -1);
-	} MEND
-
-	MSTART(1, "CompareStringA/cp1256/IgnoreCase", 6) {
-		res = CompareStringA(LOCALE_SYSTEM_DEFAULT, NORM_IGNORECASE|LOCALE_USE_CP_ACP, dest, -1, "abcdefghrtgo", -1);
-	} MEND
 
 	lstrcpyW(destW, TEST_STRINGW);
 	MSTART(1, "lstrcmpiW", 2) {
 		res = lstrcmpiW(destW, TEST_STRING2W);
 	} MEND
+
 	/* TODO
 	CompareStringW
 	*W functions
