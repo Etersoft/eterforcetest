5c264a3e3d9ef0a0b84ce5b1d25b6fa94bb31175
Vitaly Lipatov <lav@etersoft.ru>
2008-12-09 19:19:53 +0300

    add GetTickCount64, just function run, get tick via asm rdtsc

--- test_time.c
+++ test_time.c
@@ -24,16 +24,72 @@
 /*
 	GetSystemTime / GetLocalTime
 	http://msdn.microsoft.com/en-us/library/ms724950(VS.85).aspx
+
+TODO for mingw:
+#if _WIN32_WINNT >= 0x0600
+WINBASEAPI ULONGLONG   WINAPI GetTickCount64(void);
+#endif
+
 */
+
+
+typedef unsigned long long ticks;
+
+/* FIXME: fix me for other compilers */
+ticks getticks(void)
+{
+     unsigned a, d;
+     asm("cpuid");
+     asm volatile("rdtsc" : "=a" (a), "=d" (d));
+
+     return (((ticks)a) | (((ticks)d) << 32));
+}
+
+WINAPI DWORD func()
+{
+	return GetTickCount();
+}
+
+WINAPI ticks empty_func()
+{
+     unsigned a = 0x101, d = 0x202;
+     return (((ticks)a) | (((ticks)d) << 32));
+}
 
 void test_time()
 {
 	SYSTEMTIME st, lt;
+	static DWORD tc;
+	static ULONGLONG utc;
+	WINBASEAPI ULONGLONG (WINAPI *pgtc)(void);
+	static DWORD (WINAPI *pfunc)(void);
+	static ticks (WINAPI *ptfunc)(void);
+	HANDLE hModule;
 
 	printf("\n\n* * *  Time operations  * * *\n");
 
 	MSTART(1, "GetTickCount", 10) {
-		GetTickCount();
+		tc = GetTickCount();
+	} MEND
+
+	pfunc = &func; /* forbid inline func */
+	MSTART(1, "GetTickCount_func", 10) {
+		tc = func();
+	} MEND
+
+	ptfunc = &empty_func; /* forbid inline func */
+	MSTART(1, "empty_10func", 1) {
+		/* ten times */
+		utc = empty_func(); utc = empty_func();
+		utc = empty_func(); utc = empty_func();
+		utc = empty_func(); utc = empty_func();
+		utc = empty_func(); utc = empty_func();
+		utc = empty_func(); utc = empty_func();
+	} MEND
+
+	/* FIXME: detect if rdtsc using will correct */
+	MSTART(1, "getticks_via_rdtsc", 1) {
+		utc = getticks();
 	} MEND
 
 	MSTART(1, "GetLocalTime", 20) {
@@ -44,5 +100,11 @@ void test_time()
 		GetSystemTime(&st);
 	} MEND
 
+	/* Since Windows Vista / 2008 */
+	hModule = LoadLibraryA("kernel32.dll");
+	pgtc = (void*)GetProcAddress(hModule, "GetTickCount64");
+	MSTART(pgtc, "GetTickCount64", 10) {
+		utc = pgtc();
+	} MEND
 
 }
