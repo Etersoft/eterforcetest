9b08979cd2f52bcab4f3fb439d067c656245288b
Vitaly Lipatov <lav@etersoft.ru>
2009-01-05 16:36:43 +0300

    compare: realize cyrillic string comparing, test W functions

--- test_compare.c
+++ test_compare.c
@@ -1,7 +1,7 @@
 /*
  * ETERFORCETEST
  *
- * Copyright 2008 Vitaly Lipatov, Etersoft
+ * Copyright 2008,2009 Vitaly Lipatov, Etersoft
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -21,17 +21,24 @@
 #include <windows.h>
 #include "eterforcetest.h"
 
+#define BUFLEN 100
 
-static WCHAR test_stringW[] = {'A','B','C','D','E','F','G','H','R','T','P','E','N','D','O','F','P','R','O','Z','M','I','Y', '\0'};
+/* 23 chars */
+static const WCHAR test_stringW[] = {'A','B','C','D','E','F','G','H','R','T','P','E','N','D','O','F','P','R','O','Z','M','I','Y', '\0'};
 #define TEST_STRINGW test_stringW
 
-static WCHAR test_string2W[] = {'a','b','c','d','e','f','g','h','r','t','g','o','\0'};
+/* Cyrillic letters */
+static const WCHAR test_stringCyW[] = {0x410, 0x411, 0x42F, 0x414, 0x419, 0x41A, 0x41B, 0x421, 0x412, 0x413,
+	0x412, 0x414, 0x415, 0x416, 0x417, 0x418, 0x422, 0x423, 0x425, 0x424, 0x427, 0x428, 0x429, '\0'};
+static CHAR test_stringCy[BUFLEN];
+
+static const WCHAR test_string2W[] = {'a','b','c','d','e','f','g','h','r','t','g','o','\0'};
 #define TEST_STRING2W test_string2W
 
-static CHAR test1_string[] = "ABCDEFGHRTPENDOFPROZMIY";
+static CHAR test1_string[BUFLEN];
 #define TEST_STRING test1_string
 
-static CHAR test1_string2[] = "abcdefghrtgo";
+static const CHAR test1_string2[] = "abcdefghrtgo";
 #define TEST_STRING2 test1_string2
 
 
@@ -51,26 +58,53 @@ static void test_compare_lang(int lang, const char*langname)
 		res = CompareStringA(lang, NORM_IGNORECASE|LOCALE_USE_CP_ACP, TEST_STRING, -1, TEST_STRING2, -1);
 	} MEND
 }
-#define TEST_COMPARE_LANG(lang) \
-	test_compare_lang(lang, ""#lang);
+
+static void test_comparew_lang(int lang, const char*langname)
+{
+	static int res;
+	char name[100];
+	LCID lcid = MAKELCID(MAKELANGID(lang, SUBLANG_DEFAULT), SORT_DEFAULT);
+
+	sprintf(name, "CompareStringW/%s", langname + strlen("LANG_"));
+	MSTART(lang, name, 20) {
+		res = CompareStringW(lcid, 0, TEST_STRINGW, -1, TEST_STRING2W, -1);
+	} MEND
+
+	sprintf(name, "CompareStringW/ICase/%s", langname + strlen("LANG_"));
+	MSTART(lang, name, 6) {
+		res = CompareStringW(lang, NORM_IGNORECASE|LOCALE_USE_CP_ACP, TEST_STRINGW, -1, TEST_STRING2W, -1);
+	} MEND
+}
 
 void test_compare()
 {
 	int res;
-	#define BUFLEN 100
-	static WCHAR destW[BUFLEN];
-	static char dest[BUFLEN];
+
+	/* Hope we run in cp1251 locale. Prepare 8bit string */
+	WideCharToMultiByte( CP_ACP, 0, test_stringCyW, -1, test_stringCy, BUFLEN, NULL, NULL);
+	WideCharToMultiByte( CP_ACP, 0, test_stringW, -1, test1_string, BUFLEN, NULL, NULL);
+	/* printf("Test String: %s\n", test_stringCy); */
 
 	printf("\n\n* * *  Comparing operations  * * *\n");
 
-	lstrcpyA(dest, TEST_STRING);
 	MSTART(1, "lstrcmpiA", 2) {
-		res = lstrcmpiA(dest, TEST_STRING2);
+		res = lstrcmpiA(TEST_STRING, TEST_STRING2);
 	} MEND
 
 	MSTART(1, "lstrcmpA", 2) {
-		res = lstrcmpA(dest, TEST_STRING2);
+		res = lstrcmpA(TEST_STRING, TEST_STRING2);
 	} MEND
+
+	MSTART(1, "lstrcmpiW", 2) {
+		res = lstrcmpiW(TEST_STRINGW, TEST_STRING2W);
+	} MEND
+
+	MSTART(1, "lstrcmpW", 2) {
+		res = lstrcmpW(TEST_STRINGW, TEST_STRING2W);
+	} MEND
+
+#define TEST_COMPARE_LANG(lang) \
+	test_compare_lang(lang, ""#lang);
 
 	TEST_COMPARE_LANG(LANG_BELARUSIAN);
 	TEST_COMPARE_LANG(LANG_ESTONIAN);
@@ -79,26 +113,56 @@ void test_compare()
 	TEST_COMPARE_LANG(LANG_UKRAINIAN);
 	TEST_COMPARE_LANG(LANG_ENGLISH);
 	TEST_COMPARE_LANG(LANG_FRENCH);
+#undef TEST_COMPARE_LANG
 
+
+	/* Test CompareStringA */
 	MSTART(1, "CompareStringA/CURRENT", 20) {
-		res = CompareStringA(GetThreadLocale(), 0, dest, -1, TEST_STRING2, -1);
+		res = CompareStringA(GetThreadLocale(), 0, TEST_STRING, -1, TEST_STRING2, -1);
 	} MEND
 
 	MSTART(1, "CompareStringA/ICase/CURRENT", 6) {
-		res = CompareStringA(GetThreadLocale(), NORM_IGNORECASE|LOCALE_USE_CP_ACP, dest, -1, TEST_STRING2, -1);
+		res = CompareStringA(GetThreadLocale(), NORM_IGNORECASE|LOCALE_USE_CP_ACP, TEST_STRING, -1, TEST_STRING2, -1);
+	} MEND
+
+	MSTART(1, "CompareStringA/CURRENT/Cyr", 20) {
+		res = CompareStringA(GetThreadLocale(), 0, test_stringCy, -1, TEST_STRING2, -1);
+	} MEND
+
+	MSTART(1, "CompareStringA/ICase/CURRENT/Cyr", 6) {
+		res = CompareStringA(GetThreadLocale(), NORM_IGNORECASE|LOCALE_USE_CP_ACP, test_stringCy, -1, TEST_STRING2, -1);
 	} MEND
 
 
-	lstrcpyW(destW, TEST_STRINGW);
-	MSTART(1, "lstrcmpiW", 2) {
-		res = lstrcmpiW(destW, TEST_STRING2W);
+#define TEST_COMPAREW_LANG(lang) \
+	test_comparew_lang(lang, ""#lang);
+
+	TEST_COMPAREW_LANG(LANG_BELARUSIAN);
+	TEST_COMPAREW_LANG(LANG_ESTONIAN);
+	TEST_COMPAREW_LANG(LANG_LATVIAN);
+	TEST_COMPAREW_LANG(LANG_RUSSIAN)
+	TEST_COMPAREW_LANG(LANG_UKRAINIAN);
+	TEST_COMPAREW_LANG(LANG_ENGLISH);
+	TEST_COMPAREW_LANG(LANG_FRENCH);
+#undef TEST_COMPAREW_LANG
+
+
+	/* Test CompareStringW */
+	MSTART(1, "CompareStringW/CURRENT", 20) {
+		res = CompareStringW(GetThreadLocale(), 0, TEST_STRINGW, -1, TEST_STRING2W, -1);
 	} MEND
 
-	/* TODO
-	CompareStringW
-	*W functions
-	*/
-	/* TODO: с кириллицей CP1251/UCS2 */
+	MSTART(1, "CompareStringW/ICase/CURRENT", 6) {
+		res = CompareStringW(GetThreadLocale(), NORM_IGNORECASE|LOCALE_USE_CP_ACP, TEST_STRINGW, -1, TEST_STRING2W, -1);
+	} MEND
+
+	MSTART(1, "CompareStringW/CURRENT/Cyr", 20) {
+		res = CompareStringW(GetThreadLocale(), 0, test_stringCyW, -1, TEST_STRING2W, -1);
+	} MEND
+
+	MSTART(1, "CompareStringW/ICase/CURRENT/Cyr", 6) {
+		res = CompareStringW(GetThreadLocale(), NORM_IGNORECASE|LOCALE_USE_CP_ACP, test_stringCyW, -1, TEST_STRING2W, -1);
+	} MEND
 
 }
 
